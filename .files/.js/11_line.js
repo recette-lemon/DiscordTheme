85bff5b0d1900e1aeb3b4c39db4a04aee2a98b45
Manuel <m26patricio@hotmail.com>
2018-08-11 18:20:11 +0100

    Added back button to stickers.

--- .files/.js/11_line.js
+++ .files/.js/11_line.js
@@ -11,9 +11,25 @@ Discord.Line = new (function(){
 	
 	let modal = new Discord.Modal(lineContainer);
 	trigger.addEventListener("click", function(){
-		lineContainer.innerHTML = "";
+		lineContainer.removeChild(lineContainer.querySelector(".dt-line-inner"));
 		lineContainer.appendChild(lineContainer.elements);
+		lineContainer.setAttribute("root", true);
 		modal.show();
+	});
+	
+	let back = document.createElement("div");
+	back.className = "dt-line-back";
+	back.innerHTML = `
+		<svg viewBox="0 0 32 32" >
+			<path d="M28,14H8.8l4.62-4.62C13.814,8.986,14,8.516,14,8c0-0.984-0.813-2-2-2c-0.531,0-0.994,0.193-1.38,0.58l-7.958,7.958C2.334,14.866,2,15.271,2,16s0.279,1.08,0.646,1.447l7.974,7.973C11.006,25.807,11.469,26,12,26c1.188,0,2-1.016,2-2  c0-0.516-0.186-0.986-0.58-1.38L8.8,18H28c1.104,0,2-0.896,2-2S29.104,14,28,14z"/>
+		</svg>
+	`;
+	lineContainer.appendChild(back);
+	
+	back.addEventListener("click", function(){
+		lineContainer.removeChild(lineContainer.querySelector(".dt-line-inner"));
+		lineContainer.appendChild(lineContainer.elements);
+		lineContainer.setAttribute("root", true);
 	});
 	
 	this.appendTo = function(place){
@@ -68,8 +84,9 @@ Discord.Line = new (function(){
 			sticker.elements.appendChild(s);
 		}
 		sticker.addEventListener("click", function(){
-			lineContainer.innerHTML = "";
+			lineContainer.removeChild(lineContainer.querySelector(".dt-line-inner"));
 			lineContainer.appendChild(sticker.elements);
+			lineContainer.removeAttribute("root");
 		});
 		let before;
 		for(let j=0;j<lineContainer.elements.children.length;j++){
