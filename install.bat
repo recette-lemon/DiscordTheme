5f260551cb90bc4638f59ac152fcc6d9a4a214c5
BrunoFCvo <bruno.fcarvalho95@gmail.com>
2019-09-13 23:13:11 +0100

    Better install script; Uninstall script;

--- install.bat
+++ install.bat
@@ -1,29 +1,32 @@
 @echo off
 if EXIST instalation.log del instalation.log>nul
-rem #get discord executable
-rem #done with a for cuz several processes but only save the first
+:: get discord executable
+:: done with a for cuz several processes but only save the first
 for /f "skip=1 delims=" %%a IN ('wmic process where "name='discord.exe'" get ExecutablePath') do (
 	if not defined d_exe (
 		set d_exe=%%a
 	)
 )
-cls
-echo --- Installer ---
-echo.
 if "%d_exe%"=="" (
-	install_canary.bat
+	echo [ERROR!] Discord needs to be running...
+	pause>nul
 	goto EOF
 )
+cls
+
+echo --- Installer ---
+echo.
+
 echo Working...
-rem #set discord executable path
+:: set discord executable path
 for %%a IN ("%d_exe%") do set d_path=%%~dpa
-rem #get discord version from a file
+:: get discord version from a file
 for /f "tokens=7 delims=\" %%a IN ("%d_path%") do set d_ver=%%a
 for /f "tokens=2 delims=-" %%a IN ("%d_ver%") do set d_ver=%%a
-rem #set discord core path
+:: set discord core path
 set d_core=%APPDATA%\discord\%d_ver%\modules\discord_desktop_core
 
-rem #make backup if it doesnt already exist
+:: make a backup if it doesnt already exist
 if NOT EXIST "%d_core%\core.asar.bak" (
 	copy "%d_core%\core.asar" "%d_core%\core.asar.bak">nul
 	echo Backed up to "%d_core%\core.asar.bak">>instalation.log
@@ -31,12 +34,12 @@ if NOT EXIST "%d_core%\core.asar.bak" (
 	echo Backup already exists at "%d_core%\core.asar.bak">>instalation.log
 )
 
-rem #unpack asar with 7z + asar plugin
-rem #always unpack from backup to get unmodified files
+:: unpack asar with 7z + asar plugin
+:: always unpack from backup to get unmodified files
 .files\.7z\7z.exe x "%d_core%\core.asar.bak" -o"%d_core%\core\" -aoa>nul
 echo Unpacked Asar to "%d_core%\core\">>instalation.log
 
-rem #modify files to point to current dir
+:: modify files to point to current dir
 if EXIST payload.js del payload.js>nul
 for /f "delims=" %%a in (.files\payload-original.js) do (
 	set _temp=%%a
@@ -60,26 +63,29 @@ for /f "delims=" %%a in (.files\injection-original.js) do (
 )
 echo Modified injection.js to point to "%~dp0">>instalation.log
 
-rem #apply payload to mainScreenPreload
+:: apply payload to mainScreenPreload
 echo.>>"%d_core%\core\app\mainScreenPreload.js"
 for /f "delims=^ tokens=1" %%b in (payload.js) do (
 	echo %%b>>"%d_core%\core\app\mainScreenPreload.js"
 )
 echo Applied payload to "%d_core%\core\app\mainScreenPreload.js">>instalation.log
 
-rem #delete modified payload since it's in the discord code
-del payload.js
-
-rem #close discord to apply changes
+:: close discord to apply changes
 taskkill /F /im discord.exe /T>nul
 echo Killed discord processes to repack Asar>>instalation.log
 
-rem #pack asar
+:: pack asar
 .files\.7z\7z a "%d_core%\core.asar" "%d_core%\core\*">nul
 echo Repacked "%d_core%\core.asar">>instalation.log
 
-rem #run discord manually cuz fuck batch scripts
+:: cleanup
+del payload.js
+rmdir /s /q "%d_core%\core"
+
 echo.
 echo --- SUCCESS ---
+
+:: restart discord
 start "" "%d_path%Discord.exe"
+
 :EOF
