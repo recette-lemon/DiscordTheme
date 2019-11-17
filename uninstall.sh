dba5724cc94a48057d7acf0c6fb54b8d16a66c0c
eiko <me@eiko.cc>
2019-11-17 03:59:24 +0000

    updated updater so it finds install location like the installer

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "starting
 finding asar location"
-cd "$HOME/.config/discord/"
-cd "$(ls | grep -E ".\..\.." | sort | tail -n 1)/modules/discord_desktop_core"
+cd "$HOME/.config/discord/" &> /dev/null || cd "$HOME"/snap/discord/??/.config/discord
+cd "$(ls | grep -E ".\..\.." | sort | tail -n 1)/modules/discord_desktop_core" &> /dev/null || { echo >&2 "asar location not found. exiting"; exit 1; }
 
 echo "replacing core.asar with backup"
 rm core.asar
