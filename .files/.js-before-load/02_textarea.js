b1cb12ccdef55329cfba4f46c34174cb5072d475
masterzagh <39348232+masterzagh@users.noreply.github.com>
2019-12-07 16:57:21 +0000

    Fixed new shadow textarea update.

--- .files/.js-before-load/02_textarea.js
+++ .files/.js-before-load/02_textarea.js
@@ -21,29 +21,6 @@ window.addEventListener('keydown', e => {
 		e.stopImmediatePropagation();
 	}
 	
-	// The rest of this code removes any reference to the current text message
-	// on variables hidden throughout the DOM and on the localStorage
-	
-	t.blur(); // Blur textarea beforehand because discord resets message on blur
-	t.value = t.defaultValue = t.textContent = t.innerHTML = newValue;
-	t.style.height = "";
-	t._wrapperState.initialValue = newValue;
-	t._valueTracker.setValue(newValue);
-	
-	react = t.getReact();
-	react.memoizedProps.value = react.pendingProps.value = newValue;
-	
-	let parent = t.closest('[class*="channelTextArea-"]');
-	react = parent.getReact();
-	react.memoizedProps.value = react.memoizedState.prefix = react.stateNode.state.prefix = newValue;
-	
-	let form = t.closest("form");
-	react = form.getReact();
-	react.memoizedState.textValue = react.stateNode.state.textValue = newValue;
-	
-	let draftStore = JSON.parse(_localStorage.DraftStore);
-	delete draftStore._state[channel];
-	_localStorage.DraftStore = JSON.stringify(draftStore);
-	t.focus(); // Focus textarea so user doesn't lose control 
-	
+	document.execCommand("selectAll");
+	document.execCommand("delete");
 }, true);
