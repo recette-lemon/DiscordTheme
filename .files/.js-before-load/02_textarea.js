d2693889c3b282978899f6e3b2597e8b32e5c670
masterzagh <39348232+masterzagh@users.noreply.github.com>
2019-12-12 20:28:57 +0000

    Fixed commands not sending last letter

--- .files/.js-before-load/02_textarea.js
+++ .files/.js-before-load/02_textarea.js
@@ -2,14 +2,19 @@ window.addEventListener('keydown', e => {
 	let newValue = "";
 	let t = e.target;
 	let channel = discord.getCurrentChannel();
-	if(e.key!='Enter') return;
+	if(e.key!='Enter' || e.ctrlKey || e.altKey || e.shiftKey) return;
 	if(!t.matches || !t.matches('[class*="channelTextArea-"] [class*="textArea-"], [class*="channelTextArea-"] [class*="slateTextArea-"]')) return;
+	
 	let channelTextArea = t.closest('[class*="channelTextArea-"]');
 	let inner = channelTextArea.children[0];
 	if(inner.querySelector('[class*="autocomplete-"]')) return;
+	
+	document.execCommand("selectAll");
+	let textValue = window.getSelection().toString();
+	
 	let innerReact = inner.getReact();
 	let innerProps = innerReact.memoizedProps;
-	let message = innerProps.preprocessInsertedText(innerProps.textValue);
+	let message = innerProps.preprocessInsertedText(textValue);
 	
 	// Quick fix for help command
 	// Can't think of a better way right now
@@ -26,6 +31,7 @@ window.addEventListener('keydown', e => {
 		e.stopImmediatePropagation();
 	}
 	
-	document.execCommand("selectAll");
 	document.execCommand("delete");
+	if(newValue)
+		document.execCommand("insertText", false, newValue);
 }, true);
