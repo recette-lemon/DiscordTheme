11ca0baa779a8dc582e59a2efda90555577a087e
masterzagh <39348232+masterzagh@users.noreply.github.com>
2020-02-29 19:49:59 +0000

    Intercept discord's message parser for good. Fixed settings position.

--- .files/.js-before-load/02_textarea.js
+++ .files/.js-before-load/02_textarea.js
@@ -1,55 +1,50 @@
-window.addEventListener('keydown', e => {
-	let newValue = "";
-	let t = e.target;
-	let channel = discord.getCurrentChannel();
-	if(e.key!='Enter' || e.ctrlKey || e.altKey || e.shiftKey) return;
-	if(!t.matches || !t.matches('[class*="channelTextArea-"] [class*="textArea-"], [class*="channelTextArea-"] [class*="slateTextArea-"]')) return;
-	
-	let channelTextArea = t.closest('[class*="channelTextArea-"]');
-	let inner = channelTextArea.children[0];
-	if(channelTextArea.querySelector('[class*="autocomplete-"]')) return;
-	
-	let range = saveSelection();
-	document.execCommand("selectAll");
-	let textValue = getStringSelection();
-	
-	let innerReact = inner.getReact();
-	let innerProps = innerReact.memoizedProps;
-	
-	//let message = innerProps.preprocessInsertedText(textValue);
-	let message = textValue;
-	
-	// Quick fix for help command
-	// Can't think of a better way right now
-	if(message.startsWith("/help")) return;
-	
-	// Trying to run command
-	let data = commands.run(message, channel);
-	if(!data && Discord.Settings.Raw.MessageModifiers.MessageModifiers.Modifiers){ // Not a command AND message modifiers are active
-		newValue = Discord.MessageModifiers.modify(Discord.Settings.Raw.MessageModifiers.MessageModifiers.Modifiers, message);
-	}else if(!data){ // Not a command
-		restoreSelection(range);
-		return; // Return since there's no need to do anything
-	}else{ // Is a command
-		e.preventDefault();
-		e.stopImmediatePropagation();
+(function(){
+	// Intercept discord code to proxy message parse library
+	let _call = Function.prototype.call;
+	Function.prototype.call = function(){
+		// Start by calling the function since I need the changes to the arguments
+		let result = _call.apply(this, arguments);
+		
+		let args = [...arguments];
+		let thisArg = args.shift();
+		let argumentsList = args;
+		
+		// Check if arguments follow a similar pattern to that being called by discord
+		// Also check if unparse was defined by the function call
+		if(thisArg && argumentsList[0] && thisArg == argumentsList[0].exports && thisArg.default && thisArg.default.unparse){
+			let def = thisArg.default;
+			
+			// Intercept message parsing and call own message parser
+			let _parse = def.parse;
+			def.parse = function(){
+				let result = _parse.apply(this, arguments);
+				let channel = arguments[0];
+				let message = result.content;
+				result.content = parseMessage(channel, result.content);
+				return result;
+			}
+			
+			// After proxying message parser undo function call interception
+			Function.prototype.call = _call;
+		}
+		
+		return result;
 	}
 	
-	document.execCommand("delete");
-	if(newValue)
-		document.execCommand("insertText", false, newValue);
-}, true);
-
-function saveSelection() {
-	let sel = window.getSelection();
-	return sel.getRangeAt(0);
-}
-function restoreSelection(range) {
-	let sel = window.getSelection();
-	sel.removeAllRanges();
-	sel.addRange(range);
-}
-function getStringSelection(){
-	let sel = window.getSelection();
-	return sel.toString();
-}
+	function parseMessage(channel, message){
+		// Quick fix for help command
+		// Can't think of a better way right now
+		if(message.startsWith("/help")) return message;
+		
+		// Trying to run command
+		let data = commands.run(message, channel.id);
+		if(!data && Discord.Settings.Raw.MessageModifiers.MessageModifiers.Modifiers){ // Not a command AND message modifiers are active
+			return Discord.MessageModifiers.modify(Discord.Settings.Raw.MessageModifiers.MessageModifiers.Modifiers, message);
+		}else if(!data){ // Not a command
+			return message;
+		}
+		
+		// If it's a command return empty message so discord blocks the message
+		return "";
+	}
+})();
