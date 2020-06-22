bab61d26473deccf316c1eda34f08e9abe50cb2b
masterzagh <39348232+masterzagh@users.noreply.github.com>
2020-06-22 21:32:53 +0100

    Removed danger notice and console errors.

--- .files/.js-before-load/01_force.js
+++ .files/.js-before-load/01_force.js
@@ -6,3 +6,13 @@ Object.defineProperty(window, "_zstdEnabled", {
 		return false;
 	}
 });
+
+(function(){
+	let error = console.error;
+	console.error = function(e){
+		// Stops annoying "corrupt instalation" console error
+		if(typeof e === 'string' && e === 'Potentially corrupt installation:')
+			return;
+		error.apply(this, arguments);
+	}
+})();
--- .files/.js-before-load/02_textarea.js
+++ .files/.js-before-load/02_textarea.js
@@ -4,16 +4,16 @@
 	Function.prototype.call = function(){
 		// Start by calling the function since I need the changes to the arguments
 		let result = _call.apply(this, arguments);
-		
+
 		let args = [...arguments];
 		let thisArg = args.shift();
 		let argumentsList = args;
-		
+
 		// Check if arguments follow a similar pattern to that being called by discord
 		// Also check if unparse was defined by the function call
 		if(thisArg && argumentsList[0] && thisArg == argumentsList[0].exports && thisArg.default && thisArg.default.unparse){
 			let def = thisArg.default;
-			
+
 			// Intercept message parsing and call own message parser
 			let _parse = def.parse;
 			def.parse = function(){
@@ -23,19 +23,19 @@
 				result.content = parseMessage(channel, result.content);
 				return result;
 			}
-			
+
 			// After proxying message parser undo function call interception
 			Function.prototype.call = _call;
 		}
-		
+
 		return result;
 	}
-	
+
 	function parseMessage(channel, message){
 		// Quick fix for help command
 		// Can't think of a better way right now
 		if(message.startsWith("/help")) return message;
-		
+
 		// Trying to run command
 		let data = commands.run(message, channel.id);
 		if(!data && Discord.Settings.Raw.MessageModifiers.MessageModifiers.Modifiers){ // Not a command AND message modifiers are active
@@ -43,7 +43,7 @@
 		}else if(!data){ // Not a command
 			return message;
 		}
-		
+
 		// If it's a command return empty message so discord blocks the message
 		return "";
 	}
