a52c5a4bad44a0c5ce4f85e5cfdab82770ca0311
masterzagh <39348232+masterzagh@users.noreply.github.com>
2021-05-29 22:47:15 +0100

    new update install

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-if [ ! -x "$(command -v asar)" ]; then
-	echo >&2 "asar not installed."
-	exit 1
-fi
-
 theme_root="$(pwd)"
 
 echo "finding asar location"
@@ -14,29 +9,16 @@ chmod -w quotes.json
 cd "$(ls | grep -E ".\..\.." | sort | tail -n 1)/modules/discord_desktop_core" &> /dev/null || { echo >&2 "asar location not found. exiting"; exit 1; }
 asar_location="$(pwd)"
 
-if [ ! -f core.asar.backup ]; then
-	cp core.asar core.asar.backup
-	echo "created core.asar.backup"
+if [ ! -f index.js.backup ]; then
+	cp index.js index.js.backup
+	echo "created index.js.backup"
 fi
 
-echo "extracting core.asar.backup"
-rm -r /tmp/discord_theme &> /dev/null
-mkdir /tmp/discord_theme
-asar e core.asar.backup /tmp/discord_theme
-
-echo "creating injection.js and deploying payload"
+echo "creating injection.js and deploying preload and index"
 cd "$theme_root/.files"
 sed "s|{{PATH}}|$theme_root/|" injection-original.js > injection.js
-sed "s|{{FILE}}|$theme_root/.files/injection.js|" payload-original.js >> /tmp/discord_theme/app/mainScreenPreload.js
-
-sed -r -i "s/contextBridge\.exposeInMainWorld\('(.+?)', (.+?)\)/window\.\1 = \2/" /tmp/discord_theme/app/mainScreenPreload.js
-sed -i -e "s/contextIsolation: true/contextIsolation: false/" -e "s/worldSafeExecuteJavaScript: false/worldSafeExecuteJavaScript: true/" /tmp/discord_theme/app/mainScreen.js
-
-echo "repacking asar"
-rm "$asar_location/core.asar"
-asar p /tmp/discord_theme "$asar_location/core.asar"
-
-echo "cleaning up tmp files"
-rm -r /tmp/discord_theme &> /dev/null
+sed "s|{{FILE}}|$theme_root/.files/injection.js|" preload-original.js >> "$asar_location/preload.js"
+cp index.js "$asar_location/index.js"
+cp -r .node/* "$asar_location/"
 
 echo "done"
