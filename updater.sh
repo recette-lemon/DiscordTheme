cd0457e3d8f4645b1b8101f0a124bc18f4bd150a
nazrin <git_r9qQYJPZ@protonmail.com>
2021-06-04 10:05:37 +0000

    made the updater not fucking shitty

--- updater.sh
+++ updater.sh
@@ -1,17 +1,2 @@
 #!/usr/bin/env bash
-echo "
-echo 'downloading zip archive'
-wget -P /tmp/ 'https://github.com/recette-lemon/DiscordTheme/archive/master.zip' &> /dev/null
-
-echo 'unzipping'
-unzip -u /tmp/master.zip -d /tmp/ > /dev/null
-
-echo 'overwriting files'
-cp -r /tmp/DiscordTheme-master/* .
-
-echo 'cleaning up'
-rm -r /tmp/DiscordTheme-master
-rm /tmp/master.zip
-
-echo 'running installer'
-chmod +x install.sh && ./install.sh" | bash
+git pull && ./install.sh
